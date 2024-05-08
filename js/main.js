7e0c6fcb2af1bd50473933b6543c7bf02b4619e8
JUSTINRR88 <126489355+JUSTINRR88@users.noreply.github.com>
2024-05-08 13:13:25 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let div_principal = document.querySelector(".root");
+
+div_principal.innerHTML = `
+<header class="header"></header>
+<main class="principal"></main>
+<div class="categorias"></div>
+<footer class="footer"></footer>
+`;
