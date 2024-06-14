807a3147b5b27b15516f47cab9b9c24275b84348
Nyulian122 <ysgaitan@scl.edu.gt>
2024-06-14 08:39:44 -0600

    estilo resposive

--- Cargar footer/cargar_footer.js
+++ Cargar footer/cargar_footer.js
@@ -12,8 +12,6 @@ div_footer.innerHTML= `
                         <a href="#"><i class="fab fa-facebook"></i></a>
                         <a href="#"><i class="fab fa-instagram"></i></a>
                         <a href="#"><i class="fab fa-youtube"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-linkedin"></i></a>
                       </div>
                   </div>
 
@@ -37,28 +35,7 @@ div_footer.innerHTML= `
                         <a href="#">Help</a>
                      </div>
                   </div>
-
-                  <div class="col" id="contact">
-                      <h3>Contact</h3>
-                      <div class="contact-details">
-                         <i class="fa fa-location"></i>
-                         <p>FF-42, Procube Avenue, <br> NY, USA</p>
-                      </div>
-                      <div class="contact-details">
-                         <i class="fa fa-phone"></i>
-                         <p>+1-8755856858</p>
-                      </div>
                   </div>
             </div>
-
-            <div class="row">
-                  <div class="form">
-                    <form action="">
-                        <input type="text" placeholder="Email here...">
-                        <button><i class="fa fa-paper-plane"></i></button>
-                    </form>
-                  </div>
-            </div>
-
         </div>
 `
